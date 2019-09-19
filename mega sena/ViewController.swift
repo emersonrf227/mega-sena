c3d9d7ba0cb181b6d25cfc523b8e086613586467
FIAP <FIAP>
2019-09-18 21:05:11 -0300

    Projeto inicial

--- mega sena/ViewController.swift
+++ mega sena/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import Intents
+import IntentsUI
+import CoreServices
+import CoreSpotlight
+
+struct UserActivitytype {
+    static let GenerateMegaSenaNumbers = "br.com.ilikeweb.megasena"
+}
 
 class ViewController: UIViewController {
-
+    
+    
+    lazy var activity: NSUserActivity = {
+        
+        let activity = NSUserActivity(activityType: UserActivitytype.GenerateMegaSenaNumbers)
+        
+        
+        let attributes = CSSearchableItemAttributeSet(itemContentType: kUTTypeItem as String)
+        
+        attributes.contentDescription = "Gerador de Números da MegaSena"
+        attributes.thumbnailData = UIImage(named: "ball")?.jpegData(compressionQuality: 1.0)
+        
+        activity.contentAttributeSet = attributes
+        activity.title = "Gerador MegaSena"
+        activity.isEligibleForSearch = true
+        activity.isEligibleForPrediction = true
+        activity.persistentIdentifier = UserActivitytype.GenerateMegaSenaNumbers
+        activity.suggestedInvocationPhrase = "Gerar número MegaSena"
+        
+        return activity
+    }()
+    
+    
+    
+    
+    
+    @IBOutlet var balls: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        genereteNumber()
     }
-
-
+    
+    func genereteNumber()  {
+        
+        var numbers: [Int] = []
+        
+        while numbers.count < 6 {
+            
+            let number = Int.random(in: 1...60)
+            if numbers.contains(number){
+                
+                numbers.append(number)
+            }
+            
+        }
+        
+        numbers.sort()
+        
+        for(index, number) in numbers.enumerated(){
+            balls[index].setTitle("\(number)", for: .normal)
+            
+            
+        }
+        
+        
+        userActivity = activity
+        userActivity?.becomeCurrent()
+    }
 }
+
 
